1aa9e64b473fbb0ef6c92be0ced2675a350dd6f4
saadghani <saadhku2@gmail.com>
2018-11-02 18:02:17 +0800

    added models and template files

--- ASP/settings.py
+++ ASP/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'airsupply.apps.AirsupplyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- airsupply/models.py
+++ airsupply/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+
+    description = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+    weight = models.DecimalField()
+    image = models.CharField(max_length=250)
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
+    status = models.CharField(max_length=100)
+    totalWeight = models.DecimalField()
+    timeOrdered = models.DateTimeField()
+    timeDelivered = models.DateTimeField()
+    timeDispatched = models.DateTimeField()
