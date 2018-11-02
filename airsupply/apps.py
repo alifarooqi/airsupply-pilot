766e0a3628b7952d135d9dea29f676ea5b661cb0
saadghani <saadhku2@gmail.com>
2018-11-02 16:39:57 +0800

    created app - airsupply. got rid of order as we will have one big app rather than small apps for each role. since itll make stuff complicated

---
+++ airsupply/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AirsupplyConfig(AppConfig):
+    name = 'airsupply'
--- order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-app_name = 'music'
-
-urlpatterns = [
-    #/order/
-    path('', views.browse, name='browse'),
-]
