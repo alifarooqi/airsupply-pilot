5a95ef761c9663f03a4426c50b02270c3326bc46
AbeerArora123 <u3534366@connect.hku.hk>
2018-11-21 06:43:22 +0800

    Import error

--- airsupply/admin.py
+++ airsupply/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.utils.html import format_html
+from django.urls import re_path
 from .models import Item, Category, Order, Cart, LineItem, Place, InterPlaceDistance, DroneLoad, ClinicManager
 
 
